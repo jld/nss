6afe6dfacc10ffc2c2435fefbb4a98de61fa7cec
Jed Davis <jld@mozilla.com>
2016-10-10 14:34:00 -0600

    Use the submodule of boringssl instead of cloning a new one every test run.

--- tests/bogo/bogo.sh
+++ tests/bogo/bogo.sh
@@ -22,11 +22,7 @@ bogo_init()
 
   mkdir -p "${HOSTDIR}/bogo"
   cd "${HOSTDIR}/bogo"
-  BORING=${BORING:=boringssl}
-  if [ ! -d "$BORING" ]; then
-    git clone -q https://boringssl.googlesource.com/boringssl "$BORING"
-    git -C "$BORING" checkout -q e34bcc91c07c0bf65ecc53a814d51f5246007150
-  fi
+  BORING=${SOURCE_DIR:?}/external_tests/boringssl
 
   SCRIPTNAME="bogo.sh"
   html_head "bogo test"
@@ -45,7 +41,7 @@ export PATH=$PATH:/usr/lib/go-1.6/bin
 cd "$(dirname "$0")"
 SOURCE_DIR="$PWD"/../..
 bogo_init
-(cd "$BORING"/ssl/test/runner;
+(cd "${BORING:?}"/ssl/test/runner;
  GOPATH="$PWD" go test -pipe -shim-path "${BINDIR}"/nss_bogo_shim \
 	 -loose-errors -allow-unimplemented \
 	 -shim-config "${SOURCE_DIR}/external_tests/nss_bogo_shim/config.json") \
